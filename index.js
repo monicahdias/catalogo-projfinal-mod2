6409b57791712ffc53d99e06e252192ba1fd7c0d
Christopher Ventura <chrisventura@outlook.com.br>
2022-03-27 11:35:37 -0300

    .

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
+const express = require("express");
+const { url } = require("inspector");
+const path = require("path");
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
+const app = express();
+
+const port = process.env.PORT || 3001;
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded());
+
+app.get("/", (req, res) => {
+  res.render("index") // render the index.ejs file
 })
 
-app.listen(3000)
+app.listen(port, () =>
+    console.log(`Servidor rodando em http://localhost:${port}`)
+);
